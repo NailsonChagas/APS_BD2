e0f5c45c91427db4e425cd5268cc80b8b166aa5c
LuizKramer <luizeduardockramer@gmail.com>
2025-07-02 15:13:22 -0300

    feat(Data): Add data insertion

---
+++ SQL/populate_tables.sql
@@ -0,0 +1,128 @@
+-- CLIENTES
+INSERT INTO Clientes (cpf_cnpj, nome, endereco, telefone, email) VALUES ('10496582305', 'Mariana Andrade', 'Fazenda Montenegro, Vale Do Jatoba, 83863794 Teixeira / SE', '+55 (031) 6542-3511', 'wbrito@example.com');
+INSERT INTO Clientes (cpf_cnpj, nome, endereco, telefone, email) VALUES ('94053871620', 'Laís Pires', 'Campo Aparecida, 24, Barroca, 16475255 Rios / GO', '+55 11 9283 2764', 'ayllavargas@example.org');
+INSERT INTO Clientes (cpf_cnpj, nome, endereco, telefone, email) VALUES ('30592817695', 'Theo Gonçalves', 'Conjunto Thales Souza, 784, São Lucas, 53287-101 Pires / CE', '(011) 9166-9784', 'rodriguesian@example.net');
+INSERT INTO Clientes (cpf_cnpj, nome, endereco, telefone, email) VALUES ('84502319651', 'Dra. Julia Montenegro', 'Vereda da Rosa, 393, Milionario, 88095701 das Neves de Goiás / AL', '+55 41 9117-1822', 'knovaes@example.com');
+INSERT INTO Clientes (cpf_cnpj, nome, endereco, telefone, email) VALUES ('86345129755', 'Stella Machado', 'Campo Fernandes, 9, Fazendinha, 01031051 da Mota / RO', '(084) 3829-9737', 'gcorreia@example.org');
+INSERT INTO Clientes (cpf_cnpj, nome, endereco, telefone, email) VALUES ('65937048148', 'Sra. Bianca da Cruz', 'Praia de Carvalho, 52, Pindura Saia, 17810801 Cavalcante / PA', '(084) 7360-2606', 'maria46@example.net');
+INSERT INTO Clientes (cpf_cnpj, nome, endereco, telefone, email) VALUES ('87219605340', 'Sra. Ana Laura Aragão', 'Esplanada de Araújo, 212, Unidas, 36193990 Leão / RO', '0800 985 4353', 'isabela24@example.com');
+INSERT INTO Clientes (cpf_cnpj, nome, endereco, telefone, email) VALUES ('51034976206', 'Julia Cardoso', 'Núcleo da Paz, Conjunto Capitão Eduardo, 84980-841 da Luz / AM', '+55 51 8244 9353', 'mirella74@example.org');
+INSERT INTO Clientes (cpf_cnpj, nome, endereco, telefone, email) VALUES ('16408297331', 'Pedro Miguel Novaes', 'Condomínio Barbosa, 18, Floresta, 98262-045 Fogaça / DF', '+55 51 1586 9232', 'juliaalves@example.org');
+INSERT INTO Clientes (cpf_cnpj, nome, endereco, telefone, email) VALUES ('56321074934', 'Guilherme Marques', 'Fazenda Alves, 66, Itatiaia, 45868501 Cavalcanti Verde / GO', '+55 (051) 1965 5698', 'gomesmateus@example.com');
+
+-- FORNECEDORES
+INSERT INTO Fornecedores (cnpj, nome_empresa, localizacao, contato, email, telefone) VALUES ('06948125000124', 'Rios Ltda.', 'Lagoa Montenegro, 66, Nossa Senhora Da Conceição, 82366-299 Guerra da Prata / TO', 'Kevin da Paz', 'lorenaviana@example.net', '0500-577-7387');
+INSERT INTO Fornecedores (cnpj, nome_empresa, localizacao, contato, email, telefone) VALUES ('21475690000105', 'Rocha das Neves S.A.', 'Área da Cruz, 29, Petropolis, 93676-320 Vasconcelos / PA', 'Felipe Sampaio', 'jaragao@example.org', '+55 71 7278 8957');
+INSERT INTO Fornecedores (cnpj, nome_empresa, localizacao, contato, email, telefone) VALUES ('98674312000185', 'Machado', 'Vereda da Mota, 573, Pantanal, 43455812 da Conceição / MG', 'Srta. Hadassa Azevedo', 'vjesus@example.net', '(061) 6036-6909');
+INSERT INTO Fornecedores (cnpj, nome_empresa, localizacao, contato, email, telefone) VALUES ('67029345000149', 'Mendonça Câmara Ltda.', 'Parque Fernandes, 82, Sion, 98069901 Carvalho dos Dourados / CE', 'José Pedro Garcia', 'otavio75@example.com', '41 4641 7080');
+INSERT INTO Fornecedores (cnpj, nome_empresa, localizacao, contato, email, telefone) VALUES ('53169048000194', 'Almeida Pinto S.A.', 'Viaduto Ian Nunes, 57, Vila São Dimas, 29912-419 das Neves / RJ', 'Maria Flor Gonçalves', 'dompastor@example.net', '+55 51 1491 9058');
+
+-- PRODUTOS
+INSERT INTO Produto (id_produto, nome, descricao, preco) VALUES ('789100000000', 'Nobis', 'Architecto officia accusamus in laudantium inventore.', 323.32);       
+INSERT INTO Produto (id_produto, nome, descricao, preco) VALUES ('789100000001', 'Magni', 'Provident ipsa quasi vitae earum repellat modi.', 22.26);
+INSERT INTO Produto (id_produto, nome, descricao, preco) VALUES ('789100000002', 'Perspiciatis', 'Ipsa quasi eaque sequi rerum assumenda sunt.', 144.76);
+INSERT INTO Produto (id_produto, nome, descricao, preco) VALUES ('789100000003', 'Exercitationem', 'Necessitatibus pariatur excepturi laudantium eveniet laborum explicabo.', 119.37);
+INSERT INTO Produto (id_produto, nome, descricao, preco) VALUES ('789100000004', 'Facilis', 'Eius nisi sunt nam maxime deleniti.', 370.87);
+INSERT INTO Produto (id_produto, nome, descricao, preco) VALUES ('789100000005', 'Excepturi', 'Laborum quidem porro officia laudantium recusandae.', 341.58);     
+INSERT INTO Produto (id_produto, nome, descricao, preco) VALUES ('789100000006', 'Consectetur', 'Dolore porro esse neque labore.', 447.17);
+INSERT INTO Produto (id_produto, nome, descricao, preco) VALUES ('789100000007', 'Quia', 'Incidunt quia ex praesentium earum dolorum.', 52.6);
+INSERT INTO Produto (id_produto, nome, descricao, preco) VALUES ('789100000008', 'Cumque', 'Et quia veritatis porro deleniti.', 216.74);
+INSERT INTO Produto (id_produto, nome, descricao, preco) VALUES ('789100000009', 'Perspiciatis', 'Quam voluptate minima eligendi aut.', 24.6);
+INSERT INTO Produto (id_produto, nome, descricao, preco) VALUES ('789100000010', 'Earum', 'Dolorem debitis accusamus modi earum.', 117.13);
+INSERT INTO Produto (id_produto, nome, descricao, preco) VALUES ('789100000011', 'Asperiores', 'Eos velit odit quidem aut accusamus eligendi.', 257.62);
+INSERT INTO Produto (id_produto, nome, descricao, preco) VALUES ('789100000012', 'Perferendis', 'Exercitationem vitae dolorum similique beatae aperiam.', 23.0);  
+INSERT INTO Produto (id_produto, nome, descricao, preco) VALUES ('789100000013', 'Porro', 'Neque placeat corrupti repellat.', 107.43);
+INSERT INTO Produto (id_produto, nome, descricao, preco) VALUES ('789100000014', 'Incidunt', 'Voluptatibus blanditiis quisquam non.', 328.44);
+
+-- ESTOQUE
+INSERT INTO Estoque (id_produto, quantidade) VALUES ('789100000000', 79);
+INSERT INTO Estoque (id_produto, quantidade) VALUES ('789100000001', 63);
+INSERT INTO Estoque (id_produto, quantidade) VALUES ('789100000002', 38);
+INSERT INTO Estoque (id_produto, quantidade) VALUES ('789100000003', 67);
+INSERT INTO Estoque (id_produto, quantidade) VALUES ('789100000004', 85);
+INSERT INTO Estoque (id_produto, quantidade) VALUES ('789100000005', 45);
+INSERT INTO Estoque (id_produto, quantidade) VALUES ('789100000006', 10);
+INSERT INTO Estoque (id_produto, quantidade) VALUES ('789100000007', 30);
+INSERT INTO Estoque (id_produto, quantidade) VALUES ('789100000008', 99);
+INSERT INTO Estoque (id_produto, quantidade) VALUES ('789100000009', 64);
+INSERT INTO Estoque (id_produto, quantidade) VALUES ('789100000010', 53);
+INSERT INTO Estoque (id_produto, quantidade) VALUES ('789100000011', 45);
+INSERT INTO Estoque (id_produto, quantidade) VALUES ('789100000012', 29);
+INSERT INTO Estoque (id_produto, quantidade) VALUES ('789100000013', 37);
+INSERT INTO Estoque (id_produto, quantidade) VALUES ('789100000014', 53);
+
+-- TRANSACOES E ITENS
+
+-- TRANSACOES E ITENS
+INSERT INTO Transacao (id_transacao, tipo_transacao, id_cliente, id_fornecedor, data, forma_pagamento, observacoes) VALUES (1, 'VENDA', '94053871620', NULL, '2025-05-25 12:46:06', 'BOLETO', 'Fugit tenetur error nihil inventore necessitatibus animi.');
+INSERT INTO ItemTransacao (id_transacao, id_produto, quantidade) VALUES (1, '789100000005', 3);
+INSERT INTO Transacao (id_transacao, tipo_transacao, id_cliente, id_fornecedor, data, forma_pagamento, observacoes) VALUES (2, 'COMPRA', NULL, '06948125000124', '2025-05-13 06:06:37', 'BOLETO', 'Numquam eaque aspernatur dolores mollitia.');
+INSERT INTO ItemTransacao (id_transacao, id_produto, quantidade) VALUES (2, '789100000014', 4);
+INSERT INTO Transacao (id_transacao, tipo_transacao, id_cliente, id_fornecedor, data, forma_pagamento, observacoes) VALUES (3, 'VENDA', '16408297331', NULL, '2025-05-18 19:37:36', 'PIX', 'Illum porro iusto.');
+INSERT INTO ItemTransacao (id_transacao, id_produto, quantidade) VALUES (3, '789100000009', 2);
+INSERT INTO ItemTransacao (id_transacao, id_produto, quantidade) VALUES (3, '789100000011', 1);
+INSERT INTO ItemTransacao (id_transacao, id_produto, quantidade) VALUES (3, '789100000000', 2);
+INSERT INTO Transacao (id_transacao, tipo_transacao, id_cliente, id_fornecedor, data, forma_pagamento, observacoes) VALUES (4, 'COMPRA', NULL, '06948125000124', '2025-05-06 18:56:08', 'CARTÃO', 'Enim eligendi similique architecto doloremque.');
+INSERT INTO ItemTransacao (id_transacao, id_produto, quantidade) VALUES (4, '789100000006', 3);
+INSERT INTO Transacao (id_transacao, tipo_transacao, id_cliente, id_fornecedor, data, forma_pagamento, observacoes) VALUES (5, 'COMPRA', NULL, '98674312000185', '2025-05-29 07:20:11', 'CARTÃO', 'Nihil rem dicta beatae dolore corrupti eaque consequuntur.');
+INSERT INTO ItemTransacao (id_transacao, id_produto, quantidade) VALUES (5, '789100000005', 2);
+INSERT INTO ItemTransacao (id_transacao, id_produto, quantidade) VALUES (5, '789100000010', 3);
+INSERT INTO ItemTransacao (id_transacao, id_produto, quantidade) VALUES (5, '789100000011', 1);
+INSERT INTO Transacao (id_transacao, tipo_transacao, id_cliente, id_fornecedor, data, forma_pagamento, observacoes) VALUES (6, 'VENDA', '16408297331', NULL, '2025-05-09 09:50:46', 'CARTÃO', 'Recusandae nisi repudiandae ullam.');
+INSERT INTO ItemTransacao (id_transacao, id_produto, quantidade) VALUES (6, '789100000007', 4);
+INSERT INTO ItemTransacao (id_transacao, id_produto, quantidade) VALUES (6, '789100000004', 5);
+INSERT INTO Transacao (id_transacao, tipo_transacao, id_cliente, id_fornecedor, data, forma_pagamento, observacoes) VALUES (7, 'VENDA', '65937048148', NULL, '2025-06-15 06:10:52', 'DINHEIRO', 'Iusto esse illo earum.');
+INSERT INTO ItemTransacao (id_transacao, id_produto, quantidade) VALUES (7, '789100000013', 1);
+INSERT INTO ItemTransacao (id_transacao, id_produto, quantidade) VALUES (7, '789100000012', 3);
+INSERT INTO Transacao (id_transacao, tipo_transacao, id_cliente, id_fornecedor, data, forma_pagamento, observacoes) VALUES (8, 'COMPRA', NULL, '98674312000185', '2025-06-25 01:07:39', 'DINHEIRO', 'Ipsa nisi suscipit quasi nobis ad dolorem.');
+INSERT INTO ItemTransacao (id_transacao, id_produto, quantidade) VALUES (8, '789100000014', 5);
+INSERT INTO ItemTransacao (id_transacao, id_produto, quantidade) VALUES (8, '789100000014', 3);
+INSERT INTO Transacao (id_transacao, tipo_transacao, id_cliente, id_fornecedor, data, forma_pagamento, observacoes) VALUES (9, 'VENDA', '51034976206', NULL, '2025-06-28 11:11:25', 'BOLETO', 'Veritatis voluptatum perferendis earum aliquam quidem iusto officiis.');
+INSERT INTO ItemTransacao (id_transacao, id_produto, quantidade) VALUES (9, '789100000002', 3);
+INSERT INTO ItemTransacao (id_transacao, id_produto, quantidade) VALUES (9, '789100000002', 2);
+INSERT INTO ItemTransacao (id_transacao, id_produto, quantidade) VALUES (9, '789100000011', 5);
+INSERT INTO ItemTransacao (id_transacao, id_produto, quantidade) VALUES (9, '789100000008', 3);
+INSERT INTO Transacao (id_transacao, tipo_transacao, id_cliente, id_fornecedor, data, forma_pagamento, observacoes) VALUES (10, 'COMPRA', NULL, '53169048000194', '2025-05-30 15:01:52', 'BOLETO', 'Voluptatum ipsa repellendus voluptas.');
+INSERT INTO ItemTransacao (id_transacao, id_produto, quantidade) VALUES (10, '789100000003', 2);
+INSERT INTO ItemTransacao (id_transacao, id_produto, quantidade) VALUES (10, '789100000008', 4);
+INSERT INTO ItemTransacao (id_transacao, id_produto, quantidade) VALUES (10, '789100000001', 1);
+INSERT INTO Transacao (id_transacao, tipo_transacao, id_cliente, id_fornecedor, data, forma_pagamento, observacoes) VALUES (11, 'VENDA', '30592817695', NULL, '2025-06-18 06:04:58', 'CARTÃO', 'Illum eum autem cum illum libero.');
+INSERT INTO ItemTransacao (id_transacao, id_produto, quantidade) VALUES (11, '789100000009', 1);
+INSERT INTO ItemTransacao (id_transacao, id_produto, quantidade) VALUES (11, '789100000006', 4);
+INSERT INTO ItemTransacao (id_transacao, id_produto, quantidade) VALUES (11, '789100000009', 4);
+INSERT INTO ItemTransacao (id_transacao, id_produto, quantidade) VALUES (11, '789100000008', 3);
+INSERT INTO Transacao (id_transacao, tipo_transacao, id_cliente, id_fornecedor, data, forma_pagamento, observacoes) VALUES (12, 'VENDA', '94053871620', NULL, '2025-06-11 12:00:02', 'PIX', 'Alias at dolorum blanditiis quo sint repudiandae.');
+INSERT INTO ItemTransacao (id_transacao, id_produto, quantidade) VALUES (12, '789100000001', 3);
+INSERT INTO ItemTransacao (id_transacao, id_produto, quantidade) VALUES (12, '789100000006', 2);
+INSERT INTO ItemTransacao (id_transacao, id_produto, quantidade) VALUES (12, '789100000007', 1);
+INSERT INTO Transacao (id_transacao, tipo_transacao, id_cliente, id_fornecedor, data, forma_pagamento, observacoes) VALUES (13, 'COMPRA', NULL, '53169048000194', '2025-05-03 17:33:41', 'CARTÃO', 'Iure amet maxime numquam libero ipsam amet.');
+INSERT INTO ItemTransacao (id_transacao, id_produto, quantidade) VALUES (13, '789100000013', 3);
+INSERT INTO Transacao (id_transacao, tipo_transacao, id_cliente, id_fornecedor, data, forma_pagamento, observacoes) VALUES (14, 'VENDA', '30592817695', NULL, '2025-05-29 13:09:56', 'PIX', 'Consectetur enim sunt laborum adipisci occaecati reiciendis.');
+INSERT INTO ItemTransacao (id_transacao, id_produto, quantidade) VALUES (14, '789100000008', 5);
+INSERT INTO ItemTransacao (id_transacao, id_produto, quantidade) VALUES (14, '789100000014', 1);
+INSERT INTO Transacao (id_transacao, tipo_transacao, id_cliente, id_fornecedor, data, forma_pagamento, observacoes) VALUES (15, 'COMPRA', NULL, '67029345000149', '2025-05-18 23:55:38', 'DINHEIRO', 'Mollitia mollitia at voluptatibus totam.');
+INSERT INTO ItemTransacao (id_transacao, id_produto, quantidade) VALUES (15, '789100000014', 3);
+INSERT INTO Transacao (id_transacao, tipo_transacao, id_cliente, id_fornecedor, data, forma_pagamento, observacoes) VALUES (16, 'COMPRA', NULL, '21475690000105', '2025-06-17 10:07:51', 'DINHEIRO', 'Officia culpa ut eos.');
+INSERT INTO ItemTransacao (id_transacao, id_produto, quantidade) VALUES (16, '789100000014', 5);
+INSERT INTO ItemTransacao (id_transacao, id_produto, quantidade) VALUES (16, '789100000001', 1);
+INSERT INTO Transacao (id_transacao, tipo_transacao, id_cliente, id_fornecedor, data, forma_pagamento, observacoes) VALUES (17, 'COMPRA', NULL, '06948125000124', '2025-05-30 04:00:20', 'CARTÃO', 'Rerum voluptatem doloribus mollitia fugit ullam.');
+INSERT INTO ItemTransacao (id_transacao, id_produto, quantidade) VALUES (17, '789100000010', 4);
+INSERT INTO ItemTransacao (id_transacao, id_produto, quantidade) VALUES (17, '789100000008', 2);
+INSERT INTO Transacao (id_transacao, tipo_transacao, id_cliente, id_fornecedor, data, forma_pagamento, observacoes) VALUES (18, 'COMPRA', NULL, '53169048000194', '2025-06-21 23:09:59', 'BOLETO', 'Sint eius ratione impedit.');
+INSERT INTO ItemTransacao (id_transacao, id_produto, quantidade) VALUES (18, '789100000014', 5);
+INSERT INTO ItemTransacao (id_transacao, id_produto, quantidade) VALUES (18, '789100000012', 2);
+INSERT INTO Transacao (id_transacao, tipo_transacao, id_cliente, id_fornecedor, data, forma_pagamento, observacoes) VALUES (19, 'COMPRA', NULL, '67029345000149', '2025-06-21 21:59:38', 'PIX', 'Reprehenderit eligendi error nesciunt voluptatum.');
+INSERT INTO ItemTransacao (id_transacao, id_produto, quantidade) VALUES (19, '789100000014', 5);
+INSERT INTO ItemTransacao (id_transacao, id_produto, quantidade) VALUES (19, '789100000007', 1);
+INSERT INTO ItemTransacao (id_transacao, id_produto, quantidade) VALUES (19, '789100000003', 2);
+INSERT INTO ItemTransacao (id_transacao, id_produto, quantidade) VALUES (19, '789100000001', 3);
+INSERT INTO Transacao (id_transacao, tipo_transacao, id_cliente, id_fornecedor, data, forma_pagamento, observacoes) VALUES (20, 'VENDA', '56321074934', NULL, '2025-06-20 03:49:26', 'CARTÃO', 'Ipsum laborum ex quis.');
+INSERT INTO ItemTransacao (id_transacao, id_produto, quantidade) VALUES (20, '789100000000', 1);
+INSERT INTO ItemTransacao (id_transacao, id_produto, quantidade) VALUES (20, '789100000011', 1);
+
+-- PROMOCOES
+INSERT INTO Promocoes (nome_promocao, data_inicio, data_fim, porcentagem, produtos_em_promocao, ativa) VALUES ('O direito de ganhar simplesmente', '2025-06-14 05:06:29', '2025-06-29 02:12:15', 15.3, ARRAY['789100000013'], TRUE);
+INSERT INTO Promocoes (nome_promocao, data_inicio, data_fim, porcentagem, produtos_em_promocao, ativa) VALUES ('A simplicidade de atingir seus objetivos de maneira eficaz', '2025-06-18 01:34:54', '2025-06-30 13:06:37', 19.87, ARRAY['789100000010', '789100000004'], TRUE);
+INSERT INTO Promocoes (nome_promocao, data_inicio, data_fim, porcentagem, produtos_em_promocao, ativa) VALUES ('O poder de evoluir sem preocupação', '2025-06-04 10:00:37', '2025-06-08 11:33:14', 26.84, ARRAY['789100000014', '789100000011'], TRUE);
+INSERT INTO Promocoes (nome_promocao, data_inicio, data_fim, porcentagem, produtos_em_promocao, ativa) VALUES ('O prazer de concretizar seus projetos mais rapidamente', '2025-06-28 03:39:24', '2025-07-10 05:50:11', 44.71, ARRAY['789100000012', '789100000007', '789100000003'], TRUE);
+INSERT INTO Promocoes (nome_promocao, data_inicio, data_fim, porcentagem, produtos_em_promocao, ativa) VALUES ('O conforto de atingir seus objetivos direto da fonte', '2025-06-10 02:43:23', '2025-06-12 22:18:49', 23.32, ARRAY['789100000001'], TRUE);
--- main.py
+++ main.py
@@ -12,9 +12,14 @@ NUM_PRODUTOS = 15
 NUM_TRANSACOES = 20
 TIPOS_TRANSACAO = ['VENDA', 'COMPRA']
 
+clientes = []  # <-- Armazenar os CPFs dos clientes
+fornecedores = []  # <-- Armazenar os CNPJs dos fornecedores
+
+print("-- CLIENTES")
 # Gerar clientes
 for _ in range(NUM_CLIENTES):
     cpf = fake.cpf().replace('.', '').replace('-', '')
+    clientes.append(cpf)
     nome = fake.name()
     endereco = fake.address().replace('\n', ', ')
     telefone = fake.phone_number()
@@ -25,6 +30,7 @@ print("\n-- FORNECEDORES")
 # Gerar fornecedores
 for _ in range(NUM_FORNECEDORES):
     cnpj = fake.cnpj().replace('.', '').replace('/', '').replace('-', '')
+    fornecedores.append(cnpj)
     nome_empresa = fake.company()
     localizacao = fake.address().replace('\n', ', ')
     contato = fake.name()
@@ -49,27 +55,65 @@ for i in range(NUM_PRODUTOS):
     print(f"INSERT INTO Estoque (id_produto, quantidade) VALUES ('{id_produto}', {quantidade});")
 
 print("\n-- TRANSACOES E ITENS")
-# Simular transações e itens
-for i in range(NUM_TRANSACOES):
+
+print("\n-- TRANSACOES E ITENS")
+id_transacao_atual = 1  # ID manual da transação
+
+for _ in range(NUM_TRANSACOES):
     tipo = random.choice(TIPOS_TRANSACAO)
+    
     if tipo == 'VENDA':
-        cliente_id = fake.cpf().replace('.', '').replace('-', '')
+        cliente_id = random.choice(clientes)  # Escolher um CPF existente
         fornecedor_id = 'NULL'
     else:
         cliente_id = 'NULL'
-        fornecedor_id = fake.cnpj().replace('.', '').replace('/', '').replace('-', '')
+        fornecedor_id = random.choice(fornecedores)  # Escolher um CNPJ existente
 
-    valor_total = 0
     data = fake.date_time_between(start_date='-60d', end_date='now')
     forma_pagamento = random.choice(['DINHEIRO', 'CARTÃO', 'PIX', 'BOLETO'])
     observacoes = fake.sentence()
-    
-    print(f"INSERT INTO Transacao (tipo_transacao, id_cliente, id_fornecedor, data, forma_pagamento, observacoes) VALUES ('{tipo}', {f"'{cliente_id}'" if cliente_id != 'NULL' else 'NULL'}, {f"'{fornecedor_id}'" if fornecedor_id != 'NULL' else 'NULL'}, '{data}', '{forma_pagamento}', '{observacoes}');")
 
-    # Simular itens da transação
+    print(
+        f"INSERT INTO Transacao (id_transacao, tipo_transacao, id_cliente, id_fornecedor, data, forma_pagamento, observacoes) "
+        f"VALUES ({id_transacao_atual}, '{tipo}', "
+        f"{f"'{cliente_id}'" if cliente_id != 'NULL' else 'NULL'}, "
+        f"{f"'{fornecedor_id}'" if fornecedor_id != 'NULL' else 'NULL'}, "
+        f"'{data}', '{forma_pagamento}', '{observacoes}');"
+    )
+
+    # Itens da transação
     qtd_itens = random.randint(1, 4)
     for _ in range(qtd_itens):
         id_produto = f"{789100000000 + random.randint(0, NUM_PRODUTOS - 1)}"
         quantidade = random.randint(1, 5)
-        print(f"-- Suponha que o último id_transacao foi {i+1}")
-        print(f"INSERT INTO ItemTransacao (id_transacao, id_produto, quantidade) VALUES ({i+1}, '{id_produto}', {quantidade});")
+        print(
+            f"INSERT INTO ItemTransacao (id_transacao, id_produto, quantidade) "
+            f"VALUES ({id_transacao_atual}, '{id_produto}', {quantidade});"
+        )
+
+    id_transacao_atual += 1
+
+print("\n-- PROMOCOES")
+
+NUM_PROMOCOES = 5  # quantidade de promoções que deseja gerar
+
+for id_promocao in range(1, NUM_PROMOCOES + 1):
+    nome_promocao = fake.catch_phrase()
+    
+    # Gerar data de início e fim válidas
+    data_inicio = fake.date_time_between(start_date='-30d', end_date='now')
+    data_fim = fake.date_time_between(start_date=data_inicio + timedelta(days=1), end_date=data_inicio + timedelta(days=15))
+    
+    porcentagem = round(random.uniform(5, 50), 2)  # entre 5% e 50%
+    
+    # Selecionar de 1 a 4 produtos para aplicar promoção
+    qtd_produtos = random.randint(1, 4)
+    produtos_ids = [f"{789100000000 + random.randint(0, NUM_PRODUTOS - 1)}" for _ in range(qtd_produtos)]
+    produtos_ids = list(set(produtos_ids))  # remover duplicatas
+    
+    produtos_sql_array = "ARRAY[" + ", ".join(f"'{pid}'" for pid in produtos_ids) + "]"
+
+    print(
+        f"INSERT INTO Promocoes (nome_promocao, data_inicio, data_fim, porcentagem, produtos_em_promocao, ativa) "
+        f"VALUES ('{nome_promocao}', '{data_inicio}', '{data_fim}', {porcentagem}, {produtos_sql_array}, TRUE);"
+    )
